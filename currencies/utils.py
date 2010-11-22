c53d29099408fecdb0ac0342a2764543609809b4
Panos Laganakos <panos.laganakos@gmail.com>
2010-11-22 02:09:39 +0200

    forgot to add the utils.py file.

---
+++ currencies/utils.py
@@ -0,0 +1,15 @@
+from decimal import *
+from django.conf import settings
+from currencies.models import Currency
+
+
+def calculate_price(price, currency):
+	try:
+		factor = Currency.objects.get(code__exact=currency).factor
+	except Currency.DoesNotExist:
+		if settings.DEBUG:
+			raise Currency.DoesNotExist
+		else:
+			factor = Decimal('0.0')
+	new_price = Decimal(price) * factor
+	return new_price.quantize(Decimal('.01'), rounding=ROUND_UP)
