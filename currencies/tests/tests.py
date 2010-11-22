0b4043a8211c09d96f12ad8fe695c7747b665c1c
Panos Laganakos <panosl@Menzoberanzan.local>
2010-11-22 12:49:08 +0200

    pep8 cleanups.

--- currencies/tests/tests.py
+++ currencies/tests/tests.py
@@ -6,11 +6,10 @@ from currencies.models import Currency
 from currencies.utils import calculate_price
 
 
-
 class UtilsTest(TestCase):
 	fixtures = ['test_data.json']
 	use_transaction = False
-	
+
 	def test_calculate_price_success(self):
 		res = calculate_price('10', 'USD')
 		self.assertEqual(res, Decimal('15.00'))
@@ -18,7 +17,7 @@ class UtilsTest(TestCase):
 	def test_calculate_price_fail(self):
 		res = calculate_price('10', 'GBP')
 		self.assertEqual(res, Decimal('0.00'))
-	
+
 	def test_calculate_price_currency_doesnotexist(self):
 		settings.DEBUG = True
 		self.assertRaises(Currency.DoesNotExist, calculate_price, '10', 'GBP')
