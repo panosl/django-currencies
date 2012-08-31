fb557ca3941bc7d62d1284bcad57d11a04546ec5
Jerome Leclanche <adys.wh@gmail.com>
2012-08-31 14:17:34 +0100

    Support searching currencies

--- currencies/admin.py
+++ currencies/admin.py
@@ -4,5 +4,6 @@ from currencies.models import Currency
 
 class CurrencyAdmin(admin.ModelAdmin):
     list_display = ("name", "is_default", "code", "symbol", "factor")
+    search_fields = ("name", "code")
 
 admin.site.register(Currency, CurrencyAdmin)
