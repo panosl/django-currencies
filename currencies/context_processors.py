40c5f5ec789cd820666596244d3e748fa9539732
Jerome Leclanche <adys.wh@gmail.com>
2012-08-31 14:33:44 +0100

    Remove an old debug comment

--- currencies/context_processors.py
+++ currencies/context_processors.py
@@ -5,7 +5,6 @@ def currencies(request):
     currencies = Currency.objects.all()
 
     if not request.session.get('currency'):
-        #request.session['currency'] = Currency.objects.get(code__exact='EUR')
         request.session['currency'] = Currency.objects.get(is_default__exact=True)
 
     return {
