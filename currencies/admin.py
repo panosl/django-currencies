89f7a2740deaf047dc5fce0a5212b6b31c849f16
Jerome Leclanche <adys.wh@gmail.com>
2012-09-02 06:20:59 +0100

    Add is_active and is_base to the admin

--- currencies/admin.py
+++ currencies/admin.py
@@ -3,7 +3,8 @@ from currencies.models import Currency
 
 
 class CurrencyAdmin(admin.ModelAdmin):
-    list_display = ("name", "is_default", "code", "symbol", "factor")
+    list_display = ("name", "is_active", "is_base", "is_default", "code", "symbol", "factor")
+    list_filter = ("is_active", )
     search_fields = ("name", "code")
 
 admin.site.register(Currency, CurrencyAdmin)
