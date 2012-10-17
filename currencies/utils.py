989418ec420be7b8e368c2a1cc3fb48a4d69c15e
stelzzz <psz@avist.biz>
2012-10-17 22:49:14 +0600

    Add convert multicurrency function
    Example of use:
    price=convert(100, "RUB", "UAH")
    Convert 100 Russian Rubles to Ukrainian Hryvnia (via base USD currency)

--- currencies/utils.py
+++ currencies/utils.py
@@ -14,3 +14,12 @@ def calculate_price(price, currency):
     price = price * currency.factor
 
     return price.quantize(Decimal("0.01"), rounding=ROUND_UP)
+
+    
+def convert(amount, from_, to):
+    amount = Decimal(amount)
+    from_currency = Currency.objects.get(code__exact=from_)
+    to_currency = Currency.objects.get(code__exact=to)
+    amount = amount * (to_currency.factor / from_currency.factor)  
+    
+    return amount.quantize(Decimal("0.01"), rounding=ROUND_UP)
