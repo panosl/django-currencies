f3fb53087ba55e409e3acfd7c4003470b96489cd
Shahal Meril <shahal@singular.net>
2016-10-02 16:33:28 +0300

    updatecurrencies now supports historical rates

--- currencies/management/commands/updatecurrencies.py
+++ currencies/management/commands/updatecurrencies.py
@@ -1,63 +1,88 @@
 # -*- coding: utf-8 -*-
 
+import datetime
 from decimal import Decimal as D
-from datetime import datetime as d
+from optparse import make_option
 
 from django.conf import settings
-from django.core.management.base import NoArgsCommand
+from django.core.management.base import BaseCommand
 from django.core.exceptions import ImproperlyConfigured
 
 from openexchangerates import OpenExchangeRatesClient
 
-from ...models import Currency as C
+from ...models import Currency, DailyCurrencyExchangeRate
 from ...utils import get_open_exchange_rates_app_id
 
 
 APP_ID = get_open_exchange_rates_app_id()
 
 
-class Command(NoArgsCommand):
-    help = "Update the currencies against the current exchange rates"
+class Command(BaseCommand):
+    help = "Gets / updates exchange rates against the base currency for a specific date."
+
+    option_list = BaseCommand.option_list + (
+        make_option('--force', '-f', action='store_true', default=False,
+                    help='Import daily rates even if already in database.'
+                    ),
+        make_option('--date', '-d', default=False,
+                    help='Import daily rates even if already in database.',
+                    ),
+    )
 
     def handle(self, *args, **options):
         self.verbose = int(options.get('verbosity', 0))
         self.options = options
 
+        if options["date"]:
+            date = datetime.datetime.strptime(options["date"], "%Y-%m-%d")
+        else:
+            date = datetime.date.today()
+
+        if DailyCurrencyExchangeRate.objects.filter(datetime__range=(date, date + datetime.timedelta(days=1)))[:1] and not options["force"]:
+            raise Exception("Data for %s already retrieved. Use '--force=1' to override." % datetime.date.strftime(date, "%Y-%m-%d"))
+
         client = OpenExchangeRatesClient(APP_ID)
         if self.verbose >= 1:
-            self.stdout.write("Querying database at %s" % (client.ENDPOINT_CURRENCIES))
+            self.stdout.write("Querying database at %s" % (client.ENDPOINT_HISTORICAL))
 
         try:
-            code = C._default_manager.get(is_base=True).code
-        except C.DoesNotExist:
+            code = Currency._default_manager.get(is_base=True).code
+        except Currency.DoesNotExist:
             code = 'USD'  # fallback to default
 
-        l = client.latest(base=code)
+        l = client.historical(base=code, date=date)
+        report_datetime = datetime.datetime.fromtimestamp(l["timestamp"]).strftime("%Y-%m-%d %H:%M:%S")
 
         if self.verbose >= 1 and "timestamp" in l:
-            self.stdout.write("Rates last updated on %s" % (
-                d.fromtimestamp(l["timestamp"]).strftime("%Y-%m-%d %H:%M:%S")))
+            self.stdout.write("Rates last updated on %s" % report_datetime)
 
         if "base" in l:
             if self.verbose >= 1:
                 self.stdout.write("All currencies based against %s" % (l["base"]))
 
-            if not C._default_manager.filter(code=l["base"]):
+            if not Currency._default_manager.filter(code=l["base"]):
                 self.stderr.write(
                     "Base currency %r does not exist! Rates will be erroneous without it." % l["base"])
             else:
-                base = C._default_manager.get(code=l["base"])
+                base = Currency._default_manager.get(code=l["base"])
                 base.is_base = True
                 base.save()
 
-        for c in C._default_manager.all():
+        for c in Currency._default_manager.all():
             if c.code not in l["rates"]:
                 self.stderr.write("Could not find rates for %s (%s)" % (c, c.code))
                 continue
 
             factor = D(l["rates"][c.code]).quantize(D(".0001"))
-            if c.factor != factor:
-                if self.verbose >= 1:
-                    self.stdout.write("Updating %s rate to %f" % (c, factor))
 
-                C._default_manager.filter(pk=c.pk).update(factor=factor)
+            if self.verbose >= 1:
+                self.stdout.write("Updating %s rate to %f" % (c, factor))
+
+            exchange_rate = DailyCurrencyExchangeRate.objects\
+                .filter(currency=c)\
+                .filter(datetime__range=(date, date + datetime.timedelta(days=1)))
+
+            if exchange_rate:
+                exchange_rate.update(factor=factor, datetime=report_datetime)
+            else:
+                DailyCurrencyExchangeRate.objects.create(currency=c, factor=factor, datetime=report_datetime)
