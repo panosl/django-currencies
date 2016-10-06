8b42f0b983153c51b6a9411d68dc8d0adf43bc70
Matthew Crenshaw <sgt@squig.gs>
2016-10-06 12:57:48 -0400

    using `code` as primary key

--- currencies/models.py
+++ currencies/models.py
@@ -11,7 +11,7 @@ from .managers import CurrencyManager
 class Currency(models.Model):
 
     code = models.CharField(_('code'), max_length=3,
-                            unique=True, db_index=True)
+                            primary_key=True)
     name = models.CharField(_('name'), max_length=35,
                             unique=True, db_index=True)
     symbol = models.CharField(_('symbol'), max_length=4, blank=True,
