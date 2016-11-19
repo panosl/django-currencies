3370332d123b0e4094f2ba2b67ddc539d5a2f090
Richard Case <rich@shiny.local>
2016-11-19 21:01:55 +0000

    added quantize parameter & consolidated implementation

--- currencies/utils.py
+++ currencies/utils.py
@@ -1,29 +1,24 @@
 # -*- coding: utf-8 -*-
 
 from decimal import Decimal as D, ROUND_UP
-
 from .models import Currency as C
 from .conf import SESSION_KEY
 
 
-def calculate(price, code):
-    to, default = C.active.get(code=code), C.active.default()
 
-    # First, convert from the default currency to the base currency,
-    # then convert from the base to the given currency
-    price = (D(price) / default.factor) * to.factor
-
-    return price.quantize(D("0.01"), rounding=ROUND_UP)
-
-
-def convert(amount, from_code, to_code):
+def convert(amount, from_code, to_code, quantize_to=D("0.01")):
     if from_code == to_code:
       return amount
 
     from_, to = C.active.get(code=from_code), C.active.get(code=to_code)
 
     amount = D(amount) * (to.factor / from_.factor)
-    return amount.quantize(D("0.01"), rounding=ROUND_UP)
+    return amount.quantize(quantize_to, rounding=ROUND_UP)
+
+
+def calculate(price, *args, **kwargs):
+    default_code = C.active.default().code
+    return convert(price, default_code, *args, **kwargs)
 
 
 def get_currency_code(request):
