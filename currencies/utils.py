96bfc279a8cb9434c24bd3769b81cce26348de9b
racitup <rich@racitup.com>
2017-05-26 13:10:06 +0100

    Fixed missing decimal exception import

--- currencies/utils.py
+++ currencies/utils.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from decimal import Decimal as D, ROUND_UP
+from decimal import Decimal as D, InvalidOperation, ROUND_UP
 from .models import Currency as C
 from .conf import SESSION_KEY
 
