5e26de5f3dddcdb65382f45e0bf8de14e9e9e8ea
racitup <rich@racitup.com>
2017-10-14 18:50:07 +0100

    made cookie fix consistent with get_currency_code but replaced SESSION_KEY with valid cookie name

--- currencies/conf.py
+++ currencies/conf.py
@@ -3,4 +3,4 @@
 from django.conf import settings
 
 SESSION_PREFIX = getattr(settings, 'CURRENCY_SESSION_PREFIX', 'session')
-SESSION_KEY = '%s:currency_code' % SESSION_PREFIX
+SESSION_KEY = '%s.currency_code' % SESSION_PREFIX
