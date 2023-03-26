434840f3a00d945b7a944749a747a45649860955
ron <ron.s@singular.net>
2023-03-26 12:55:38 +0300

    Support python 3.8 / django 4 upgrade

--- currencies/models.py
+++ currencies/models.py
@@ -2,12 +2,10 @@
 
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from django.utils.encoding import python_2_unicode_compatible
 
 from .managers import CurrencyManager, ExchangeRatesManager
 
 
-@python_2_unicode_compatible
 class Currency(models.Model):
 
     code = models.CharField(_('code'), max_length=3, db_index=True)
